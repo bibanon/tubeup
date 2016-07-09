b6fb91a33efa9bcdfd900760c19323144c7b8951
Paul Henning <vxbinaca@gmail.com>
2016-07-09 17:49:06 -0400

    Didn't notice I disabled archiving. Fixed. Soz.

--- tubeup.py
+++ tubeup.py
@@ -67,7 +67,7 @@ def download(URLs, proxy_url):
     
     ydl_opts = {
         'outtmpl': 'downloads/%(title)s-%(id)s.%(ext)s',
-#        'download_archive': 'downloads/.ytdlarchive', ## I guess we will avoid doing this because it prevents failed uploads from being redone in our current system. Maybe when we turn it into an OOP library?
+        'download_archive': 'downloads/.ytdlarchive', ## I guess we will avoid doing this because it prevents failed uploads from being redone in our current system. Maybe when we turn it into an OOP library?
         'restrictfilenames': True,
 #       'verbose': True,		## We only care about errors not successes, anything else is pollution
         'progress_with_newline': True,
