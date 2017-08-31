09552c8097a4ae24f2bfa59a539b57cabd2a5b8f
Antonizoon <antonizoon@bibanon.org>
2017-08-30 22:00:55 -0400

    version 0.0.9
    Signed-off-by: Antonizoon <antonizoon@bibanon.org>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tubeup',
-    version='0.0.8',
+    version='0.0.9',
     url='https://github.com/bibanon/tubeup',
     license='GPL 3',
     author='Bibliotheca Anonoma',
