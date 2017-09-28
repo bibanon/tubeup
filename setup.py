07c5d355c301f375cab466f41f44bbb83d4f287c
Antonizoon <antonizoon@bibanon.org>
2017-09-28 10:41:44 -0400

    version 0.0.11

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tubeup',
-    version='0.0.10',
+    version='0.0.11',
     url='https://github.com/bibanon/tubeup',
     license='GPL 3',
     author='Bibliotheca Anonoma',
