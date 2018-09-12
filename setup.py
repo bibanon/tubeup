4e8d7e4e735ef2cf12f77adc8390d0f7c418efda
Paul Henning <vxbinaca@protonmail.ch>
2018-09-11 21:47:42 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='tubeup',
-    version='0.0.14',
+    version='0.0.15',
     url='https://github.com/bibanon/tubeup',
     license='GPL 3',
     author='Bibliotheca Anonoma',
