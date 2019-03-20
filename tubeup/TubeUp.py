eea98072690db5f433cc020fc28ab30956d0cb7d
altCensored.com <admin@altCensored.com>
2019-03-20 12:13:31 -0300

    re.sub not needed per issue #88
    https://github.com/bibanon/tubeup/issues/88

--- tubeup/TubeUp.py
+++ tubeup/TubeUp.py
@@ -276,9 +276,6 @@ class TubeUp(object):
         itemname = ('%s-%s' % (vid_meta['extractor'],
                                vid_meta['display_id']))
 
-        # Replace illegal characters within identifer
-        itemname = re.sub('\W+', '-', itemname)
-
         metadata = self.create_archive_org_metadata_from_youtubedl_meta(
             vid_meta)
 
