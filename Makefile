7cb3b606bb7546ac85c2a46bf3de19fe2b28d5af
Brandon Galbraith <brandongalbraith@users.noreply.github.com>
2020-07-02 02:33:40 -0500

    Fix Error Handling for Tags (#126)
    * Improve makefile for dev purposes
    
    * Improve Error Handling of Tags
    
    * Create test json file with null tags value
    
    * Linting
    
    * Remove VERSION hack
    
    Scraping version number is broken. TODO.

--- Makefile
+++ Makefile
@@ -3,8 +3,17 @@ VERSION=$(shell grep -m1 version setup.py | cut -d\' -f2)
 binary:
 	pex . --python=python3 --python-shebang='/usr/bin/env python3' -e tubeup.__main__:main  -o tubeup-$(VERSION)-py2.py3-none-any.pex
 
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+
 publish:
 	git tag -a v$(VERSION) -m 'version $(VERSION)'
 	git push --tags
 	python setup.py register
 	python setup.py sdist upload
+
+test: clean-pyc
+	pytest --cov
--- tubeup/TubeUp.py
+++ tubeup/TubeUp.py
@@ -459,9 +459,16 @@ class TubeUp(object):
             except:
                 print ("No categories found.")
 
-        if 'tags' in vid_meta:  # some video services don't have tags
-            for tag in vid_meta['tags']:
-                tags_string += '%s;' % tag
+        if 'tags' in vid_meta: # some video services don't have tags
+            try:
+                if 'tags' in vid_meta is None:
+                    tags_string += '%s;' % vid_meta['id']
+                    tags_string += '%s;' % 'video'
+                else:
+                    for tag in vid_meta['tags']:
+                        tags_string += '%s;' % tag
+            except:
+                print ("Unable to process tags successfully.")
 
         # license
         licenseurl = TubeUp.determine_licenseurl(vid_meta)
