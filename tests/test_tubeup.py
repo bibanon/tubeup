8cef0db8206b9b1ef178efe4cd938774c2a5993a
Chris Midgley <midgleyc@gmail.com>
2020-11-22 20:53:12 +0000

    test: use video currently on YouTube for live tests

--- tests/test_tubeup.py
+++ tests/test_tubeup.py
@@ -429,11 +429,11 @@ class TubeUpTests(unittest.TestCase):
         copy_testfiles_to_tubeup_rootdir_test()
 
         result = tu.get_resource_basenames(
-            ['https://www.youtube.com/watch?v=6iRV8liah8A'])
+            ['https://www.youtube.com/watch?v=KdsN9YhkDrY'])
 
         expected_result = {os.path.join(
             current_path, 'test_tubeup_rootdir', 'downloads',
-            'Mountain_3_-_Video_Background_HD_1080p-6iRV8liah8A')}
+            'KdsN9YhkDrY')}
 
         self.assertEqual(expected_result, result)
 
@@ -514,7 +514,7 @@ class TubeUpTests(unittest.TestCase):
 
         videobasename = os.path.join(
             current_path, 'test_tubeup_rootdir', 'downloads',
-            'Mountain_3_-_Video_Background_HD_1080p-6iRV8liah8A')
+            'KdsN9YhkDrY')
 
         copy_testfiles_to_tubeup_rootdir_test()
 
@@ -526,7 +526,7 @@ class TubeUpTests(unittest.TestCase):
                   content=b'{"over_limit": 0}',
                   headers={'content-type': 'application/json'})
 
-            m.get('https://archive.org/metadata/youtube-6iRV8liah8A',
+            m.get('https://archive.org/metadata/youtube-KdsN9YhkDrY',
                   content=b'{}',
                   headers={'content-type': 'application/json'})
 
@@ -534,37 +534,39 @@ class TubeUpTests(unittest.TestCase):
             # in mock_upload_response_by_videobasename(), so this test
             # doesn't perform upload to the real archive.org server.
             mock_upload_response_by_videobasename(
-                m, 'youtube-6iRV8liah8A', videobasename)
+                m, 'youtube-KdsN9YhkDrY', videobasename)
 
             result = list(tu.archive_urls(
-                ['https://www.youtube.com/watch?v=6iRV8liah8A']))
+                ['https://www.youtube.com/watch?v=KdsN9YhkDrY']))
 
             expected_result = [(
-                'youtube-6iRV8liah8A',
+                'youtube-KdsN9YhkDrY',
                 {'mediatype': 'movies',
-                 'creator': 'Video Background',
+                 'creator': 'RelaxingWorld',
                  'collection': 'opensource_movies',
-                 'title': 'Mountain 3 - Video Background HD 1080p',
-                 'description': ('Mountain 3 - Video Background HD 1080p<br>If '
-                                 'you use this video please put credits to my'
-                                 ' channel in description:<br>https://www.youtub'
-                                 'e.com/channel/UCWpsozCMdAnfI16rZHQ9XDg<br>© D'
-                                 'on\'t forget to SUBSCRIBE, LIKE, COMMENT an'
-                                 'd RATE. Hope you all enjoy! <br/><br/>Sourc'
-                                 'e: <a href="https://www.youtube.com/watch?v'
-                                 '=6iRV8liah8A">https://www.youtube.com/watch'
-                                 '?v=6iRV8liah8A</a><br/>Uploader: <a href="h'
-                                 'ttp://www.youtube.com/channel/UCWpsozCMdAnf'
-                                 'I16rZHQ9XDg">Video Background</a>'),
-                 'date': '2015-01-05',
-                 'year': '2015',
-                 'subject': ('Youtube;video;Entertainment;Video Background;'
-                             'Footage;Animation;Cinema;stock video footage;'
-                             'Royalty free videos;Creative Commons videos;'
-                             'free movies online;youtube;HD;1080p;Amazing '
-                             'Nature;Mountain;'),
-                 'originalurl': 'https://www.youtube.com/watch?v=6iRV8liah8A',
-                 'licenseurl': 'https://creativecommons.org/licenses/by/3.0/',
+                 'title': 'Epic Ramadan - Video Background HD1080p',
+                 'description': ('If you enjoy my work, please consider Subscribe to my NEW '
+                                 'channel for more videos: <br>'
+                                 'https://www.youtube.com/MusicForRelaxation?sub_confirmation=1 <br>'
+                                 '▷ If you use this video, please put credits to my channel '
+                                 'in description: <br>'
+                                 'Source from RelaxingWorld: https://goo.gl/HsW75m<br>'
+                                 '<br>'
+                                 '▷ Also, do not forget to Subscribe to my channel. Thanks! '
+                                 '<br/><br/>Source: <a '
+                                 'href="https://www.youtube.com/watch?v=KdsN9YhkDrY">https://www.youtube.com/watch?v=KdsN9YhkDrY</a><br/>Uploader: '
+                                 '<a '
+                                 'href="http://www.youtube.com/channel/UCWpsozCMdAnfI16rZHQ9XDg">RelaxingWorld</a>'
+                                 ),
+                 'date': '2016-06-25',
+                 'year': '2016',
+                 'subject': ('Youtube;video;Film & Animation;Video Background;'
+                             'Footage;Animation;Cinema;Royalty Free Videos;'
+                             'Stock Video Footage;Video Backdrops;'
+                             'Amazing Nature;youtube;HD;1080p;Creative Commons Videos;'
+                             'relaxing music;Ramadan;'),
+                 'originalurl': 'https://www.youtube.com/watch?v=KdsN9YhkDrY',
+                 'licenseurl': '',
                  'scanner': SCANNER})]
 
             self.assertEqual(expected_result, result)
