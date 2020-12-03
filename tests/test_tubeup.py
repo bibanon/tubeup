cf43bbf2f2faa39d0daea2b06a73de6bac5d2357
Chris Midgley <midgleyc@gmail.com>
2020-12-03 22:16:36 +0000

    dev: remove verbose required for tests to pass on travis

--- tests/test_tubeup.py
+++ tests/test_tubeup.py
@@ -417,14 +417,7 @@ class TubeUpTests(unittest.TestCase):
 
     def test_get_resource_basenames(self):
         tu = TubeUp(dir_path=os.path.join(current_path,
-                                          'test_tubeup_rootdir'),
-                    # HACK: A hack to make the test in Travis successful,
-                    # We need to investigate more about this, it doesn't
-                    # make sense that the verbose flag affect the
-                    # youtubedl extract_info() process.
-                    # See:
-                    # https://travis-ci.org/bibanon/tubeup/builds/299091640
-                    verbose=True)
+                                          'test_tubeup_rootdir'))
 
         copy_testfiles_to_tubeup_rootdir_test()
 
@@ -503,14 +496,7 @@ class TubeUpTests(unittest.TestCase):
     def test_archive_urls(self):
         tu = TubeUp(dir_path=os.path.join(current_path,
                                           'test_tubeup_rootdir'),
-                    ia_config_path=get_testfile_path('ia_config_for_test.ini'),
-                    # HACK: A hack to make the test in Travis successful,
-                    # We need to investigate more about this, it doesn't
-                    # make sense that the verbose flag affect the
-                    # youtubedl extract_info() process.
-                    # See:
-                    # https://travis-ci.org/bibanon/tubeup/builds/299091640
-                    verbose=True)
+                    ia_config_path=get_testfile_path('ia_config_for_test.ini'))
 
         videobasename = os.path.join(
             current_path, 'test_tubeup_rootdir', 'downloads',
