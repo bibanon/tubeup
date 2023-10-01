dd53f7c1aef3aac4e095853420c9d6771c5ac691
Paper <37962225+mrpapersonic@users.noreply.github.com>
2023-10-01 04:27:24 -0400

    setup.py: use stub
    this finalizes the conversion to pyproject.toml, which is the new standard most other projects are using nowadays

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
 from setuptools import setup
-import re
-import ast
 
-
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('tubeup/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
-
-setup(
-    name='tubeup',
-    version=version,
-    url='https://github.com/bibanon/tubeup',
-    license='GPL 3',
-    author='Bibliotheca Anonoma',
-    description='Youtube (and other video site) to Internet Archive Uploader',
-    packages=[
-        'tubeup',
-    ],
-    entry_points={
-        'console_scripts': [
-            'tubeup = tubeup.__main__:main',
-        ],
-    },
-    install_requires=[
-        'internetarchive',
-        'docopt==0.6.2',
-        'yt-dlp',
-    ]
-)
+setup()
