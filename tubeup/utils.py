5a1bd461ae6446ec3e38bc403ec4a8b4fa024503
Digital <34419970+DigitalDwagon@users.noreply.github.com>
2024-11-13 12:18:47 -0500

    fix: don't rely on internetarchive's cli parsing
    Fixes compatibility with internetarchive>=5.0.3 by using a custom parser for the --metadata parameter instead of relying on the one (formerly) used by internetarchive
    
    Closes #338

--- tubeup/utils.py
+++ tubeup/utils.py
@@ -1,10 +1,29 @@
 import os
 import re
+from collections import defaultdict
 
 
 EMPTY_ANNOTATION_FILE = ('<?xml version="1.0" encoding="UTF-8" ?>'
                          '<document><annotations></annotations></document>')
 
+def key_value_to_dict(lst):
+    """
+    Convert many key:value pair strings into a python dictionary
+    """
+    if not isinstance(lst, list):
+        lst = [lst]
+
+    result = defaultdict(list)
+    for item in lst:
+        key, value = item.split(":", 1)
+        assert value, f"Expected a value! for {key}"
+        if result[key] and value not in result[key]:
+            result[key].append(value)
+        else:
+            result[key] = [value]
+
+    # Convert single-item lists back to strings for non-list values
+    return {k: v if len(v) > 1 else v[0] for k, v in result.items()}
 
 def sanitize_identifier(identifier, replacement='-'):
     return re.sub(r'[^\w-]', replacement, identifier)
